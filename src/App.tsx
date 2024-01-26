0d1b2373ba3a5bdc4351d1d4bc77b8cfde9c394b
Daly <dalyna64@gmail.com>
2024-01-26 22:35:26 +0100

    ably live provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
 import { Refine } from "@refinedev/core";
 
 import "./App.css";
-import options from "./options";
 import { dataProvider } from "./providers/dataProvider";
 import Product from "./Product";
 import ProductList from "./ProductList";
-import { liveProvider } from "./providers/liveProvider";
+import { liveProvider, Ably } from "@refinedev/ably";
 
+const API_TOKEN = "syVQsA.ofJCQg:GvXwhLsJhjMo4onQ_zQKjvb9biBIXMiDd7qLo9ZVA38";
+const client = new Ably.Realtime(API_TOKEN);
 function App() {
   return (
     <Refine
       dataProvider={dataProvider}
-      liveProvider={liveProvider}
-      options={{ liveMode: "auto" }}
-      // options={options}
+      liveProvider={liveProvider(client)}
+      options={{
+        syncWithLocation: true,
+        warnWhenUnsavedChanges: true,
+        useNewQueryKeys: true,
+        projectId: "SJfTms-PYL6TL-FC8Rws",
+        liveMode: "auto",
+      }}
     >
       <h1>Hello Refine</h1>
       <hr />
--- src/options.ts
+++
@@ -1,8 +0,0 @@
-const options = {
-  syncWithLocation: true,
-  warnWhenUnsavedChanges: true,
-  useNewQueryKeys: true,
-  projectId: "SJfTms-PYL6TL-FC8Rws",
-  
-};
-export default options;
